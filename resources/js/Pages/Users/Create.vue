5e6032bd5a991931e3fa9a99eb1441827b5135e0
alicancanpolat35 <alicancanpolat35@gmail.com>
2024-05-12 02:27:00 +0300

    form examplev2

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -5,41 +5,46 @@
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">Name</label>
             <input v-model="form.name" type="text" class="border border-gray-400 p-2 w-full" name="name" id="name"  >
-            <div v-if="errors.name" v-text="errors.name" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.name" v-text="form.errors.name" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">E-Mail</label>
             <input v-model="form.email" type="text" class="border border-gray-400 p-2 w-full" name="email" id="email"  >
-            <div v-if="errors.email" v-text="errors.email" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.email" v-text="form.errors.email" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">Password</label>
             <input v-model="form.password" type="password" class="border border-gray-400 p-2 w-full" name="password" id="password"  >
-            <div v-if="errors.password" v-text="errors.password" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.password" v-text="form.errors.password" class="text-red-500 text-xs mt-1"></div>
         </div>
         <div class="mb-6">
-            <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Submit</button>
+            <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500" :disabled="form.processing">Submit</button>
         </div>
     </form>
 </template>
 
 <script setup>
-import { reactive } from "vue";
-import { router } from "@inertiajs/vue3";
+import { ref} from "vue";
+import { router,useForm } from "@inertiajs/vue3";
 
 defineProps({
    errors: Object
 });
-let form = reactive({
+
+let form = useForm({
     name: '',
     email: '',
     password: '',
 });
 
+setTimeout(() => {
+    form.reset();
+},3000)
+
 let submit1 = () => {
-    router.post('/users', form);
+    form.post('/users', form);
 }
 </script>
 
