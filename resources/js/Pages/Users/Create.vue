26908b2f9bc87e8e87d9bfe43a98e5ea11b69a65
alicancanpolat35 <alicancanpolat35@gmail.com>
2024-05-12 02:13:50 +0300

    form example

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -4,23 +4,24 @@
     <form @submit.prevent="submit1" class="max-w-md mx-auto mt-7">
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">Name</label>
-            <input v-model="form.name" type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
+            <input v-model="form.name" type="text" class="border border-gray-400 p-2 w-full" name="name" id="name"  >
+            <div v-if="errors.name" v-text="errors.name" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">E-Mail</label>
-            <input v-model="form.email" type="text" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
+            <input v-model="form.email" type="text" class="border border-gray-400 p-2 w-full" name="email" id="email"  >
+            <div v-if="errors.email" v-text="errors.email" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">Password</label>
-            <input v-model="form.password" type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
+            <input v-model="form.password" type="password" class="border border-gray-400 p-2 w-full" name="password" id="password"  >
+            <div v-if="errors.password" v-text="errors.password" class="text-red-500 text-xs mt-1"></div>
         </div>
         <div class="mb-6">
             <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Submit</button>
         </div>
-
-
     </form>
 </template>
 
@@ -28,6 +29,9 @@
 import { reactive } from "vue";
 import { router } from "@inertiajs/vue3";
 
+defineProps({
+   errors: Object
+});
 let form = reactive({
     name: '',
     email: '',
